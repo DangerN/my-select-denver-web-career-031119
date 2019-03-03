bcf571a9a5cbe78e867c722eb23df807d8d7cfb2
convivial-system-4430 <cdneal091@gmail.com>
2019-03-03 17:52:41 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,9 @@
 def my_select(collection)
- # your code here!
+ i = 0
+ rval = []
+ while i < collection.length
+ if collection[i] == (yield collection[i])
+   return yield collection[i]
+ end
+ end
 end
